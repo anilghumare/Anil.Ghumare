334e398291d04b2d31c2c1f36f36baf6b9e4dc51
anilghumare <105354744+anilghumare@users.noreply.github.com>
2022-06-16 20:56:16 +0530

    Student project updated

---
+++ spring-boot-demo1/src/main/java/com/zensar/springbootdemo/SpringBootDemo1Application.java
@@ -0,0 +1,21 @@
+package com.zensar.springbootdemo;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+public class SpringBootDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemo1Application.class, args);
+	}
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
+	}
+
+}
